30baf6e7658ab073a94ff88784ce29afe1eecb43
Yang-9987 <1013404703@qq.com>
2023-05-25 20:46:01 +0800

    Initial Commit

---
+++ OneStep/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OneStep
+//
+//  Created by Yang Jianqi on 2023/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OneStep/OneStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneStepApp.swift
+//  OneStep
+//
+//  Created by Yang Jianqi on 2023/5/25.
+//
+
+import SwiftUI
+
+@main
+struct OneStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
